e290725ac4a3c081205194c0d62922992d4f3fd4
Cody Ferguson <34279452+codyferguson@users.noreply.github.com>
2021-06-07 07:44:53 -0500

    [Change] Fix download resume, removed unused CSS, fixed grid of technologies (#2)
    * Match mobile btn color, remove contact form and other unused stuff
    
    * update resumeData and remove unused css
    
    * Fix resume download and added resume
    
    * working on fixing grid of skills to have a nice layout

--- src/Components/About.js
+++ src/Components/About.js
@@ -41,7 +41,7 @@ class About extends Component {
               </div>
               <div className="columns download">
                 <p>
-                  <a href={resumeDownload} className="button">
+                  <a href={resumeDownload} target="_blank" rel="noopener noreferrer" className="button">
                     <i className="fa fa-download"></i>Download Resume
                   </a>
                 </p>
