42864020f6c76e49dd8d571308576c4adf33acee
Cody Ferguson <34279452+codyferguson@users.noreply.github.com>
2021-11-07 11:02:24 -0600

    Small import fix (#8)
    * small import fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import ReactGA from 'react-ga';
 import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
