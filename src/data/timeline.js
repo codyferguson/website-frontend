13abfe850f3c59353ca8854cd1c37013a7d57eb9
Cody Ferguson <cfsoftwareengineer14@gmail.com>
2023-08-21 20:31:00 -0500

    Moved assets to public. Copied timeline info

---
+++ src/data/timeline.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        year: 'Current',
+        title: 'Software Engineer',
+        company: 'Alaska Airlines',
+        duration: '1 year',
+        details: 'TODO'
+    },
+    {
+        year: 'Oct 2022',
+        title: 'Software Engineer',
+        company: 'DocuSign',
+        duration: '1 year',
+        details: 'TODO'
+    },
+]
